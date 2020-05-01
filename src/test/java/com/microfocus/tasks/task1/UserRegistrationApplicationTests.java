605bd1cd4286dd136603f262b7912e71f93fb804
Jaisimha Kulkarni <jaisimha.kulkarni@microfocus.com>
2020-05-01 17:20:45 +0530

    adding files

---
+++ src/test/java/com/microfocus/tasks/task1/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microfocus.tasks.task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
